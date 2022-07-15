8a0bcf46bbd7a77cc1ba8b34d477180b7680549d
Otavin23 <alvesotavio132@gmail.com>
2022-07-14 21:37:07 -0300

    Create route Home

---
+++ src/styles/globlaStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+
+
+export const CreateGlobalStyles = createGlobalStyle`
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: "Montserrat",  sans-serif;
+    }
+
+`
